6dc1005f804212ea7e4d96ed515a35d517b27312
TEDESCO Florent <florent.tedesco@agrosupdijon.fr>
2021-11-15 16:19:33 +0100

    Set the url's validator

--- src/validators/validators.js
+++ src/validators/validators.js
@@ -1,4 +1,20 @@
-export function isUrl() {
-  // ToDo Add url's condition
-  return false
+export function isUrl(value) {
+  const regexp = new RegExp(
+    '^' +
+      '(?:https?://)?' + // Protocol
+      '(?:\\S+(?::\\S*)?@)?' + // Authentication user:pass
+      '(?:' +
+      '(?:(?:\\d{1,3})(?:\\.\\d{1,3}){3})' + // IP address
+      '|' +
+      '(?:(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)' + // Host name
+      '(?:\\.(?:[a-z\\u00a1-\\uffff0-9]-*)*[a-z\\u00a1-\\uffff0-9]+)*' + // Domain name
+      '(?:\\.(?:[a-z\\u00a1-\\uffff]{2,}))' + // TLD identifier
+      ')' +
+      '(?::\\d{2,5})?' + // Port
+      '(?:\\/[-a-zA-Z0-9()@:%_+.~#?&=]*)*' + // Path
+      '$',
+    'i'
+  )
+
+  return !!regexp.exec(value)
 }
