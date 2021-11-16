6ffac57ca5fbeb45e97394690a6904f220d42f1a
TEDESCO Florent <florent.tedesco@agrosupdijon.fr>
2021-11-16 08:23:13 +0100

    Adding tests for Import/Export

--- src/index.test.js
+++ src/index.test.js
@@ -1,7 +1,23 @@
-import { ExampleComponent } from '.'
+import {
+  Form,
+  Select,
+  TextField,
+  UrlTextField,
+  withForm,
+  withFormContext
+} from './index'
 
-describe('ExampleComponent', () => {
-  it('is truthy', () => {
-    expect(ExampleComponent).toBeTruthy()
+const components = {
+  Form,
+  Select,
+  TextField,
+  UrlTextField,
+  withForm,
+  withFormContext
+}
+
+describe('Export/Import tests', () => {
+  test.each(Object.entries(components))('%s', (name, component) => {
+    expect(component).toBeDefined()
   })
 })
