307b5b742a3c475a73becfc93fd3a80f0427d430
Florent TEDESCO <tedesco.florent.1996@live.fr>
2021-11-18 23:57:32 +0100

    Add Form's component

---
+++ src/components/Form.test.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Form from './Form'
+
+describe('Form tests', () => {
+  test('render', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <Form onSubmit={() => {}} parameters={{}}>
+        Content
+      </Form>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
