29391161017d3145cd67de4d93de84a3f556fdaf
TEDESCO Florent <florent.tedesco@agrosupdijon.fr>
2021-11-30 08:49:49 +0100

    Add Slider's tests

---
+++ src/components/fields/Slider/Slider.test.js
@@ -0,0 +1,23 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Form from '../../Form'
+import Slider from './Slider'
+
+describe('Slider tests', () => {
+  test('render', () => {
+    const div = document.createElement('div')
+    ReactDOM.render(
+      <Form
+        parameters={{
+          defaultValues: {
+            test: ''
+          }
+        }}
+      >
+        <Slider name='test' />
+      </Form>,
+      div
+    )
+    ReactDOM.unmountComponentAtNode(div)
+  })
+})
