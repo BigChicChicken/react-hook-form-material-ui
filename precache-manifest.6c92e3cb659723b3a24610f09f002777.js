59b4406e7802c09ee1f29c8a333c36d0da436e82
TEDESCO Florent <florent.tedesco@agrosupdijon.fr>
2021-12-02 08:23:54 +0100

    Updates

---
+++ precache-manifest.6c92e3cb659723b3a24610f09f002777.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b2073e6a15f60b568e25530d5d8cf5d1",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9ec593ef963044a21d73",
+    "url": "./static/css/main.68489e58.chunk.css"
+  },
+  {
+    "revision": "4bed837fa8fcbf53dfa6",
+    "url": "./static/js/2.09a6147c.chunk.js"
+  },
+  {
+    "revision": "fc32f3338f9d7343fd384dd59f7e5474",
+    "url": "./static/js/2.09a6147c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9ec593ef963044a21d73",
+    "url": "./static/js/main.bae868aa.chunk.js"
+  },
+  {
+    "revision": "ea4c46d86be27f349eb5",
+    "url": "./static/js/runtime-main.699036ba.js"
+  }
+]);
